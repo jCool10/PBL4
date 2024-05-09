fe43f9614b7382b994b8073d6b327b5a9750741f
jCool10 <valentinohoang1908@gmail.com>
2024-05-09 22:04:00 +0700

    first commit

---
+++ Instacart-Market-Basket-Analysis/plantsgo.bat
@@ -0,0 +1,8 @@
+python products_Word2Vec.py
+python products_Word2Vec_features.py
+
+python create_prior_data.py
+python create_prior_add_data.py
+
+python basket_v12_prior_and_train_data_cv5_up_up.py
+python basket_v12_prior_and_train_data_cv5_up_up_shuff.py
